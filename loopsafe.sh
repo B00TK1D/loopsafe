1f7557a9898de76981c8c73f78252bd4e6b7db32
B00TK1D <stearns.josiah@gmail.com>
2024-01-21 22:52:46 -0500

    Switched to strictly loopback (removed nat mod dependancy)

--- loopsafe.sh
+++ loopsafe.sh
@@ -1,28 +1,24 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]; then
     echo "Invalid arguments provided."
-    echo "Usage: $0 <local_port> <remote_ip> <remote_port>"
+    echo "Usage: $0 <original_port> <new_port>"
     exit 1
 fi
 
-
-echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -j MASQUERADE
-
 FORWORDING_ENABLED=0
 while true; do
-    nc -z $2 $3
+    nc -z 127.0.0.1 $3
     if [ $? -eq 1 ]; then
         if [ $FORWORDING_ENABLED -eq 1 ]; then
             echo "Remote connection to $2:$3 is down. Disabling port forwarding..."
-            iptables -t nat -D PREROUTING -p tcp --dport $1 -j DNAT --to-destination $2:$3
+            iptables -A PREROUTING -t nat -p tcp --dport $1 -j REDIRECT --to-port $2
             FORWORDING_ENABLED=0
         fi
     else
         if [ $FORWORDING_ENABLED -eq 0 ]; then
             echo "Remote connection to $2:$3 is up. Enabling port forwarding..."
-            iptables -t nat -A PREROUTING -p tcp --dport $1 -j DNAT --to-destination $2:$3
+            iptables -D PREROUTING -t nat -p tcp --dport $1 -j REDIRECT --to-port $2
             FORWORDING_ENABLED=1
         fi
     fi
