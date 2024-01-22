76782e74a593dd4205d67dcb40848384a5ef9e2a
B00TK1D <stearns.josiah@gmail.com>
2024-01-21 23:05:08 -0500

    Log first action

--- loopsafe.sh
+++ loopsafe.sh
@@ -10,13 +10,13 @@ FORWORDING_ENABLED=2
 while true; do
     nc -z 127.0.0.1 $2
     if [ $? -eq 1 ]; then
-        if [ $FORWORDING_ENABLED -eq 1 ]; then
+        if [ $FORWORDING_ENABLED -ne 0 ]; then
             echo "Remote connection to $2:$3 is down. Disabling port forwarding..."
             iptables -D PREROUTING -t nat -p tcp --dport $1 -j REDIRECT --to-port $2
             FORWORDING_ENABLED=0
         fi
     else
-        if [ $FORWORDING_ENABLED -eq 0 ]; then
+        if [ $FORWORDING_ENABLED -ne 1 ]; then
             echo "Remote connection to $2:$3 is up. Enabling port forwarding..."
             iptables -A PREROUTING -t nat -p tcp --dport $1 -j REDIRECT --to-port $2
             FORWORDING_ENABLED=1
