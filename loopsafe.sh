af1e0a9251b8eced3ce886b5d97058bd216d855b
B00TK1D <stearns.josiah@gmail.com>
2024-01-22 01:24:45 -0500

    Added missing netcat -p

--- loopsafe.sh
+++ loopsafe.sh
@@ -27,9 +27,9 @@ while true; do
         fi
     else
         if [ $FORWORDING_ENABLED -ne 1 ]; then
-            echo "Remote connection to $2:$3 is up. Enabling port forwarding..."
+            echo "Remote connection to $3:$2 is up. Enabling port forwarding..."
             if [ $# -eq 3 ]; then
-                nc -lk $2 -e nc $3 $2
+                nc -lkp $2 -e nc $3 $2
                 PROXY_PID=$!
             fi
             iptables -A PREROUTING -t nat -p tcp --dport $1 -j REDIRECT --to-port $2 2>/dev/null
