303eb064966104889f7babddea82622d84a68058
B00TK1D <stearns.josiah@gmail.com>
2024-01-29 13:54:00 -0500

    Insert rules at top of chain, fixed trap codes

--- loopsafe.sh
+++ loopsafe.sh
@@ -14,9 +14,12 @@ if [ $# -eq 3 ]; then
     REMOTE_HOST=$3
 fi
 
-trap 'kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGKILL 2>/dev/null
-trap 'kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGNINT 2>/dev/null
-trap 'kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGTERM 2>/dev/null
+PORT_FIFO="/tmp/.test-port-$$"
+PIPE_FIFO="/tmp/.test-pipe-$$"
+
+trap "kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm PORT_FIFO 2>/dev/null; rm PIPE_FIFO 2>/dev/null; echo -e \"\nDisabling port forwarding and exiting\"; exit" SIGKILL 2>/dev/null
+trap "kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm PORT_FIFO 2>/dev/null; rm PIPE_FIFO 2>/dev/null; echo -e \"\nDisabling port forwarding and exiting\"; exit" SIGNINT 2>/dev/null
+trap "kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm PORT_FIFO 2>/dev/null; rm PIPE_FIFO 2>/dev/null; echo -e \"\nDisabling port forwarding and exiting\"; exit" SIGTERM 2>/dev/null
 
 while true; do
     nc -z $REMOTE_HOST $2
@@ -25,25 +28,25 @@ while true; do
             echo "Remote connection to $3:$2 is down. Disabling port forwarding..."
             iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $PROXY_PORT 2>/dev/null
             kill $PROXY_PID 2>/dev/null
+            rm PIPE_FIFO 2>/dev/null
             PROXY_PID=-1
-            rm /tmp/.loopsafe-port-$$ 2>/dev/null
-            rm /tmp/.loopsafe-pipe-$$ 2>/dev/null
             FORWORDING_ENABLED=0
         fi
     else
         if [ $FORWORDING_ENABLED -ne 1 ]; then
             echo "Remote connection to $3:$2 is up. Enabling port forwarding..."
             if [ $# -eq 3 ]; then
-                PROXY_PORT="0"
-                mkfifo /tmp/.loopsafe-port-$$
-                mkfifo /tmp/.loopsafe-pipe-$$
-                cat /tmp/.loopsafe-pipe-$$ | nc $REMOTE_HOST $2 | nc -lkvp 0 > /tmp/.loopsafe-pipe-$$ 2>/tmp/.loopsafe-port-$$
-                while [ "$PROXY_PORT" -eq "0" ]; do
-                    PROXY_PORT=$(head -n 1 /tmp/.loopsafe-port-$$ | cut -d' ' -f4)
+                PROXY_PORT=""
+                mkfifo $PIPE_FIFO
+                cat $PIPE_FIFO | nc $REMOTE_HOST $2 | nc -lkvp 0 > $PIPE_FIFO 2>$PORT_FIFO &
+                while [ "$PROXY_PORT" = "" ]; do
+                    PROXY_PORT=$(head -n 1 $PORT_FIFO | cut -d' ' -f4)
                 done
                 PROXY_PID=$!
+                rm PORT_FIFO 2>/dev/null
+                echo "PROXY_PORT: $PROXY_PORT, PROXY_PID: $PROXY_PID"
             fi
-            iptables -A PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null
+            iptables -I PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $PROXY_PORT
             FORWORDING_ENABLED=1
         fi
     fi
