6547b084e48e3bd659375574efe977888dbab9f6
B00TK1D <stearns.josiah@gmail.com>
2024-01-29 02:14:03 -0500

    Kill proxy on exit

--- loopsafe.sh
+++ loopsafe.sh
@@ -7,16 +7,16 @@ if [ $# -ne 2 ] && [ $# -ne 3 ]; then
 fi
 
 FORWORDING_ENABLED=2
-PROXY_PID=0
+PROXY_PID=-1
 PROXY_PORT=$2
 REMOTE_HOST="127.0.0.1"
 if [ $# -eq 3 ]; then
     REMOTE_HOST=$3
 fi
 
-trap 'iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGKILL 2>/dev/null
-trap 'iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGNINT 2>/dev/null
-trap 'iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGTERM 2>/dev/null
+trap 'kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGKILL 2>/dev/null
+trap 'kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGNINT 2>/dev/null
+trap 'kill $PROXY_PID 2>/dev/null; iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $2 2>/dev/null; rm /tmp/.loopsafe-port-$$ 2>/dev/null; rm /tmp/.loopsafe-pipe-$$ 2>/dev/null; echo -e "\nDisabling port forwarding and exiting"; exit' SIGTERM 2>/dev/null
 
 while true; do
     nc -z $REMOTE_HOST $2
@@ -24,9 +24,8 @@ while true; do
         if [ $FORWORDING_ENABLED -ne 0 ]; then
             echo "Remote connection to $3:$2 is down. Disabling port forwarding..."
             iptables -D PREROUTING -t nat -p tcp ! -s $REMOTE_HOST --dport $1 -j REDIRECT --to-port $PROXY_PORT 2>/dev/null
-            if [ $PROXY_PID -ne 0 ]; then
-                kill $PROXY_PID 2>/dev/null
-            fi
+            kill $PROXY_PID 2>/dev/null
+            PROXY_PID=-1
             rm /tmp/.loopsafe-port-$$ 2>/dev/null
             rm /tmp/.loopsafe-pipe-$$ 2>/dev/null
             FORWORDING_ENABLED=0
