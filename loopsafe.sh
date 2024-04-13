6f3b5c01de13f7b83a77b03a75d3e2c4dfae1ef3
B00TK1D <stearns.josiah@gmail.com>
2024-04-12 23:10:12 -0400

    Fixed alpine dependancies

--- loopsafe.sh
+++ loopsafe.sh
@@ -9,26 +9,61 @@ fi
 # Ensure dependancies are installed
 UPDATED=0
 
-if ! $(which ncat > /dev/null); then
-    echo "Installing ncat..."
-    sudo apt-get update >/dev/null 2>&1
-    UPDATED=1
-    sudo apt-get install -y ncat >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo "Failed to install ncat.  Please install it manually and try again."
-        exit 1
+NCAT="$(which ncat)"
+
+if [ "$NCAT" = "" ]; then
+    if [ "$(nc -? 2>&1 | grep PROG)" = "" ]; then
+        echo "Installing ncat..."
+        if ! $(which apt > /dev/null); then
+            if ! $(which apk > /dev/null); then
+                echo "Failed to install ncat.  Please install it manually and try again."
+                exit 1
+            fi
+            sudo apk update >/dev/null 2>&1
+            UPDATED=1
+            sudo apk install -y ncat >/dev/null 2>&1
+            if [ $? -ne 0 ]; then
+                echo "Failed to install ncat.  Please install it manually and try again."
+                exit 1
+            fi
+        else
+            sudo apt-get update >/dev/null 2>&1
+            UPDATED=1
+            sudo apt-get install -y ncat >/dev/null 2>&1
+            if [ $? -ne 0 ]; then
+                echo "Failed to install ncat.  Please install it manually and try again."
+                exit 1
+            fi
+        fi
+    else
+        NCAT="$(which nc)"
     fi
 fi
 
+echo "Using $NCAT as netcat"
+
 if ! $(which iptables > /dev/null); then
     echo "Installing iptables..."
-    if [ $UPDATED -eq 0 ]; then
+    if ! $(which apt > /dev/null); then
+        if ! $(which apk > /dev/null); then
+            echo "Failed to install iptables.  Please install it manually and try again."
+            exit 1
+        fi
+        sudo apk update >/dev/null 2>&1
+        UPDATED=1
+        sudo apk install -y iptables >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "Failed to install iptables.  Please install it manually and try again."
+            exit 1
+        fi
+    else
         sudo apt-get update >/dev/null 2>&1
-    fi
-    sudo apt-get install -y iptables >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo "Failed to install iptables.  Please install it manually and try again."
-        exit 1
+        UPDATED=1
+        sudo apt-get install -y iptables >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "Failed to install iptables.  Please install it manually and try again."
+            exit 1
+        fi
     fi
 fi
 
@@ -44,10 +79,9 @@ if [ $# -eq 3 ]; then
     REMOTE_HOST=$3
 fi
 
-NCAT="$(which ncat)"
 
 while true; do
-    ncat -z $REMOTE_HOST $2
+    $NCAT -z $REMOTE_HOST $2
     if [ $? -eq 1 ]; then
         if [ $FORWORDING_ENABLED -ne 0 ]; then
             echo "Remote connection to $3:$2 is down. Disabling port forwarding..."
@@ -61,10 +95,10 @@ while true; do
             echo "Remote connection to $3:$2 is up. Enabling port forwarding..."
             if [ $# -eq 3 ]; then
                 PROXY_PORT=""
-                ncat -lkp 0 -e "$NCAT $REMOTE_HOST $2" &
+                $NCAT -lkp 0 -e "$NCAT $REMOTE_HOST $2" &
                 PROXY_PID=$!
                 while [ "$PROXY_PORT" = "" ]; do
-                    PROXY_PORT=$(lsof -a -itcp -p $PROXY_PID | tail -n 1 | cut -d':' -f2 | cut -d' ' -f1)
+                    PROXY_PORT=$(lsof -a -itcp -p $PROXY_PID | tail -n 1 | cut -d':' -f2 | cut -d' ' -f1 | cut -f1)
                 done
                 echo "Proxy port is $PROXY_PORT"
             fi
@@ -72,5 +106,5 @@ while true; do
             FORWORDING_ENABLED=1
         fi
     fi
-    sleep 2
+    sleep 1
 done
